1469db6c5324fafcbd1ccc09c2be984d2d3db9f4
madushkasenarathna <madushkasenarathna434@gmail.com>
2025-08-04 10:06:50 +0530

    fg

---
+++ src/main/java/com/example/springhosttesting/Controller/CustomerController.java
@@ -0,0 +1,14 @@
+package com.example.springhosttesting.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customer")
+public class CustomerController {
+    @GetMapping
+    public String CustomerController() {
+        return "Customer 1";
+    }
+}
---
+++ src/test/java/com/example/springhosttesting/SpringHostTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhosttesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHostTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
